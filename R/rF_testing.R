13813339cf89050c811e59fb08e6513c89ffc65a
samrabi1 <samrabi@utexas.edu>
2020-05-17 17:21:30 -0500

    commit

---
+++ R/rF_testing.R
@@ -0,0 +1,34 @@
+#' @title A random forest classifier training and testing function
+#'
+#' @description This function allows one to perform random forest classification (see (\code{\link[randomForest]{randomForest}}) for more information regarding the function to perform this classification method) of varying models on a training and testing set of the original data, the size of which is defined by the user.
+#'
+#' @param dat dataset to perform test on
+#' @param s proportion of dataset to be assigned to the 'training' dataset
+#' @param x the classifying variable in the dataset
+#' @param model written in the style of a regression, defines the classifying variable and the predictors the user wants to use to perform the test
+#'
+#' @keywords classification, random forest
+#'
+#' @export
+#'
+#' @examples
+#' rF_testing(dat = iris, s = 0.5, x = Species, model = Species ~ Petal.Length + Petal.Width + Sepal.Length)
+
+rF_testing <- function(dat, s, x, model) {
+
+  train <- dat %>% dplyr::sample_frac(s)
+  test <- dplyr::anti_join(dat, train)
+
+  random <- randomForest::randomForest(model, data=train)
+
+  test_mat <- stats::predict(random, newdata = test)
+  attach(test)
+  table <- table(x, test_mat)
+  detach(test)
+
+  list <- list(random$confusion, table)
+
+
+  return(list)
+
+}
